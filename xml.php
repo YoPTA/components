e8091204b24f75b9457bf30d404361f5508a21e1
YoPTA <s.nichipurenko@yandex.ru>
2017-03-14 08:08:57 +0300

    Comments added to xml.php
    1. Были добавлены комментарии в файл xml.php.

--- xml.php
+++ xml.php
@@ -3,22 +3,47 @@
 	
 	$XML_CODEPAGE = 'UTF-8';
 	$XML_VERSION = '1.0';
-	
+
+	/*
+	 * Преобразует значение в кодировку UTF-8.
+	 * @var $value value - значение
+	 * return value OR boolean
+	 */
 	function WIN1251ToUTF8($value)
 	{
 		return iconv('Windows-1251', 'UTF-8', $value);
-	}	
+	}
 
+	/*
+	 * Преобразует значение в кодировку Windows-1251.
+	 * @var $value value - значение
+	 * return value OR boolean
+	 */
 	function UTF8ToWIN1251($value)
 	{
 		return iconv('UTF-8', 'Windows-1251', $value);
 	}	
-	
+
+	/*
+	 * Преобразует значение одной кодирвоки в другую.
+	 * @var $value value - значение
+	 * @var $from string - кодировка с какой преобразует
+	 * @var $to string - кодировка в какую преобразуют
+	 * return value OR boolean
+	 */
 	function FormatXMLCodePage($value, $from, $to)
 	{
 		return ($from != $to) ? iconv($from, $to, $value) : $value;
 	}		
-	
+
+	/*
+	 * Разбиваем информацию о пространстве имен
+	 * (название пространства имен и значение)
+	 * и передаем их по указателю во входные параметры.
+	 * @var $value array() - переменная, хранящая информацию о пространстве имен
+	 * @var $namespace_name string - название пространства имен
+	 * @var $namespace_uri string - значение пространства имен
+	 */
 	function GetXMLNamespace($value, &$namespace_name, &$namespace_uri)
 	{
 		if (is_array($value))
@@ -32,19 +57,34 @@
 		   $namespace_uri = $value;			
 		}	
 	}
-	
+
+	/*
+	 * Возвращает информацию о названии пространства имен.
+	 * @var $value string - переменная, хранящая информацию о пространстве имен
+	 * return string
+	 */
 	function GetXMLNamespaceName($value)
 	{
-		GetXMLNamespace($value, $namespace_name, $namespace_name);
+		GetXMLNamespace($value, $namespace_name, $namespace_uri);
 		return $namespace_name;
-	}		
-	
+	}
+
+	/*
+	 * Возвращает значения пространства имен.
+	 * @var $value string - переменная, хранящая информацию о пространстве имен
+	 * return string
+	 */
 	function GetXMLNamespaceURI($value)
 	{
 		GetXMLNamespace($value, $namespace_name, $namespace_uri);
 		return $namespace_uri;
 	}	
-	
+
+	/*
+	 * Возвращает кодировку XML документа.
+	 * @var $xml DOMCoument - XML-документ
+	 * return string
+	 */
 	function GetXMLEncoding(&$xml)
 	{
 		global $XML_CODEPAGE;
@@ -58,8 +98,18 @@
 		
 		return ($result != '') ? $result : $XML_CODEPAGE;
 	}
-	
-	function CreateXMLNode(&$xml, $name, $value = '', $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Возвращает созданный XML элемент в зависимости от входных параметров.
+	 * Элемент может быть создан, как с пространством имен, так и без него.
+	 * @var $xml object - XML-документ
+	 * @var $name string - название тега
+	 * @var $value value - значение тега
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return object
+	 */
+	function CreateXMLNode(&$xml, $name, $value = '', $namespace = null, $codepage = 'UTF-8')
 	{	
 		if (!is_object($xml) or $name == '')
 		{
@@ -103,7 +153,18 @@
 		
 		return $result;		
 	}		
-	
+
+	/*
+	 * Копирует дерево тегов одного тега в другой.
+	 * Применимо, как в одном XML-документе, так и в нескольких.
+	 * Если происходит копия тега внутри одного XML документа, то копируется
+	 * только один тег (первый, даже если их несколько) из указанного тега-ресурса
+	 * в указанный тег-назначения.
+	 * @var $xml_src object - тег-ресурс
+	 * @var $xml_dest object - тег-назначения
+	 * @var $recursion boolean - ПОКА НЕ ПОНЯТНО
+	 * return boolean
+	 */
 	function CopyXMLNode($xml_src, $xml_dest, $recursion = true)
 	{
 		if (!is_object($xml_src) or !is_object($xml_dest))
@@ -131,7 +192,16 @@
 	}
 	
 
-	function AddXMLChild(&$xml, $name, $value = '', $namespace = null, $codepage = 'Windows-1251')
+	/*
+	 * Добавляет новый дочерный тег в указанный тег или XML-документ.
+	 * @var $xml object - XML-документ или XMl-тег
+	 * @var $name string - название для будущего тега
+	 * @var $value string - значение будущего тега
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return object
+	 */
+	function AddXMLChild(&$xml, $name, $value = '', $namespace = null, $codepage = 'UTF-8')
 	{
 		if (!is_object($xml))
 		{
@@ -142,7 +212,12 @@
 		
 		return $xml->appendChild(CreateXMLNode($owner, $name, $value, $namespace, $codepage));			
 	}
-	
+
+	/*
+	 * Удаляет XML-тег/дерево тегов.
+	 * @var $xml object - тег для удаления
+	 * return boolean
+	 */
 	function DelXMLChild(&$xml)
 	{
 		if (!is_object($xml))
@@ -160,7 +235,17 @@
 		return true;
 	}	
 
-	function GetXMLNode(&$xml, $name, $namespace = null, $codepage = 'Windows-1251')
+	/*
+	 * Возвращает тег в указанном во входном параметре XML-документе по названию
+	 * тега в указанном пространстве имен (если пространство имен не указано, то
+	 * поиск будет осуществляеться только по названию тега).
+	 * @var $xml object - XML-документ
+	 * @var $name string - название тега
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * retrun object OR boolean
+	 */
+	function GetXMLNode(&$xml, $name, $namespace = null, $codepage = 'UTF-8')
 	{
 		if (!is_object($xml))
 		{
@@ -190,8 +275,20 @@
 		
 		return $result;
 	}
-	
-	function GetXMLValue(&$xml, $name = null, $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Возвращает значение тега. Тег может быть указан в первом входном параметре
+	 * (при этом второй входной параметр должен быть пустым или его просто можно не указывать).
+	 * Так же тег может быть указан во втором входном параметре (при этом первым параметром
+	 * указывается его корневой тег (если корневого тега нет, то необходимо указать его в первом
+	 * входном параметре, как описывалось ранее)).
+	 * @var $xml object - тег
+	 * @var $name string - название тега
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return string OR boolean
+	 */
+	function GetXMLValue(&$xml, $name = null, $namespace = null, $codepage = 'UTF-8')
 	{	
 		if (!is_object($xml))
 		{
@@ -211,8 +308,20 @@
 
 		return FormatXMLCodePage($node->nodeValue, GetXMLEncoding($xml), $codepage);
 	}
-	
-	function SetXMLValue(&$xml, $name, $value, $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Устанавливает значение тега. Тег может быть указан в первом входном параметре
+	 * (при этом второй входной параметр должен быть пустым или его просто можно не указывать).
+	 * Так же тег может быть указан во втором входном параметре (при этом первым параметром
+	 * указывается его корневой тег (если корневого тега нет, то необходимо указать его в первом
+	 * входном параметре, как описывалось ранее)).
+	 * @var $xml object - тег
+	 * @var $name string - название тега
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return boolean
+	 */
+	function SetXMLValue(&$xml, $name, $value, $namespace = null, $codepage = 'UTF-8')
 	{		
 		if (!is_object($xml))
 		{
@@ -234,8 +343,16 @@
 		
 		return true;		
 	}	
-	
-	function GetXMLAttribute(&$xml, $name, $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Возвращает значение атрибута в указанном теге.
+	 * @var $xml object - тег
+	 * @var $name string - название атрибута
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return string OR boolean
+	 */
+	function GetXMLAttribute(&$xml, $name, $namespace = null, $codepage = 'UTF-8')
 	{	
 		if (!is_object($xml))
 		{
@@ -265,8 +382,17 @@
 		
 		return $result;
 	}
-	
-	function SetXMLAttribute(&$xml, $name, $value, $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Устанавливает значения атрибута для тега.
+	 * @var $xml object - тег
+	 * @var $name string - название атрибута
+	 * @var $value string - значение атрибута
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return object
+	 */
+	function SetXMLAttribute(&$xml, $name, $value, $namespace = null, $codepage = 'UTF-8')
 	{
 		if (!is_object($xml))
 		{
@@ -295,8 +421,16 @@
 		
 		return $result;
 	}
-	
-	function DelXMLAttribute(&$xml, $name, $namespace = null, $codepage = 'Windows-1251')
+
+	/*
+	 * Удаляет атрибут в указанном теге.
+	 * @var $xml object - тег
+	 * @var $name string - название атрибута
+	 * @var $namespace array() - пространство имен
+	 * @var $codepage string - кодировка
+	 * return boolean
+	 */
+	function DelXMLAttribute(&$xml, $name, $namespace = null, $codepage = 'UTF-8')
 	{
 		global $XML_NAMESPACE_URI_INDEX;
 		
